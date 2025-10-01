63da2e0beb0196e0a01926cd6447fdefdd2fb813
Lucas Zurverra <luking.dev@gmail.com>
2025-10-01 14:46:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ Hello *world!*
  
 df = pd.read_csv("car_sales_data.csv")
 
-st.bar_chart(df, x="Manufacturer", y="Manufacturer")
+st.bar_chart(df, x="Manufacturer", y="Model")
 
 st.write(df.head())
